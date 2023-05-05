86c0e9bdbb034a0a0e52266f9f7960dd8f9ec24c
MelisaRS <melisa.rs.1990@gmail.com>
2023-05-05 05:48:35 -0400

    Initializations (Setup) for servomotors, switch and button

--- Robot_AutisticKids.ino
+++ Robot_AutisticKids.ino
@@ -1,15 +1,15 @@
 //Libraries
-#include <ezButton.h> //for the button
-#include <ServoTimer2.h> //for ServoMotor, because this is Timer2 and don't have problems
+#include <ezButton.h>     //for the button
+#include <ServoTimer2.h>  //for ServoMotor, because this is Timer2 and don't have problems
 //These libraries are for DFPlayer functionalities
 //In this case we use dfplayer to have a better sound and have fewer conflicts with the servomotor
-#include <SoftwareSerial.h> 
+#include <SoftwareSerial.h>
 #include <DFRobotDFPlayerMini.h>
 
 //Variables for Servomotors
 /*creation of servomotors with the servoTimer2 library for 
 the left arm, right arm, left leg, right leg, and head.*/
-ServoTimer2 sLeftArm, sRightArm, sLeftLeg, sRightLeg, sphead;
+ServoTimer2 sLeftArm, sRightArm, sLeftLeg, sRightLeg, sHead;
 
 /*These variables are for the servomotor pins that will generate the movements 
 of the left arm, right arm, left leg, right leg, and head.*/
@@ -20,9 +20,9 @@ const int spRightLeg = 10;
 const int spHead = 11;
 
 //Angles for the servos
-const int saInitial = 750; //for 0 degree
-const int saIntermediate = 1500; //for 90 degree
-const int saFinal = 2250; //for around 180 degree
+const int saInitial = 750;        //for 0 degree
+const int saIntermediate = 1500;  //for 90 degree
+const int saFinal = 2250;         //for around 180 degree
 
 //States for the servos
 const int sStateLeftArm = saInitial;
@@ -35,13 +35,13 @@ const int sStateHead = saInitial;
 
 //Variables for Switch
 
-const int switchP = 7; //Pin for switch
+const int switchP = 7;  //Pin for switch
 //only for test //these are optional for this moment
-const int ledRedP = 2; //off
-const int ledGreenP = 2; //on
+const int ledRedP = 2;    //off
+const int ledGreenP = 2;  //on
 
 // Variables for Button
-const int buttonP = 8; //Pin for button
+const int buttonP = 8;  //Pin for button
 
 //States for the buttons
 int buttonOld = 1;
@@ -51,23 +51,42 @@ int buttonNew;
 int buttonFlag = 1;
 
 //create ezButton object for Switch
-ezButton toggleSwitch(switchP); 
+ezButton toggleSwitch(switchP);
 
 //---------------------------------------
 //times por daleys
 int dtI = 100;
 int dt = 500;
 
-///////////////////////////////////////////////
-///////////                        ///////////
+////////////////////////////////////////////////
+///////////     Initializations     ///////////
 //////////////////////////////////////////////
 
 void setup() {
-  // put your setup code here, to run once:
 
+  //For Servomotors
+  sLeftArm.attach(spLeftArm);
+  sRightArm.attach(spRightArm);
+  sLeftLeg.attach(sLeftLeg);
+  sRightLeg.attach(spRightLeg);
+  sHead.attach(spHead);
+
+  //For Switch
+  toggleSwitch.setDebounceTime(50);  // set debounce time to 50 milliseconds
+  pinMode(ledRedP, OUTPUT);
+  pinMode(ledGreenP, OUTPUT);
+
+  //For Button
+  pinMode(buttonP, INPUT);
+
+  //For Funtion Random
+  randomSeed(analogRead(A5));
+  delay(2000);
+  buttonFlag = random(1, 6);
 }
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
